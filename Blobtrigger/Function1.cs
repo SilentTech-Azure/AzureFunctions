55ca3f9af290df5b4620433286155f914a224a55
Anvi <anveshgouds@hotmail.com>
2023-09-10 08:01:16 +0530

    Blob trigger setup

---
+++ Blobtrigger/Function1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Blobtrigger
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([BlobTrigger("anvicontainer/{name}", 
+            Connection = "AzureWebJobsStorage")]Stream myBlob,
+            string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: " +
+                $"{myBlob.Length} Bytes");
+        }
+    }
+}
