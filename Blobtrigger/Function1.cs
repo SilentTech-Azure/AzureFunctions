bda25d669d93f9df9da33747b00163d2e377e2fa
Anvi <anveshgouds@hotmail.com>
2023-09-15 10:14:52 +0530

    Added Service Bus Trigger

--- Blobtrigger/Function1.cs
+++ Blobtrigger/Function1.cs
@@ -3,18 +3,22 @@ using System.IO;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+//=sing Microsoft.Azure.WebJobs.ServiceBus;
 
 namespace Blobtrigger
 {
     public class Function1
     {
         [FunctionName("Function1")]
-        public void Run([BlobTrigger("anvicontainer/{name}", 
+        [return: ServiceBus("servicebusqueue", Connection = "AzureWebJobsServiceBus")]
+        public string Run([BlobTrigger("anvicontainer/{name}", 
             Connection = "AzureWebJobsStorage")]Stream myBlob,
             string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: " +
                 $"{myBlob.Length} Bytes");
+
+            return $"Inserted File Name:- {name}";
         }
     }
 }
